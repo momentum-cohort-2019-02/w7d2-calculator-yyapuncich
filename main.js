fb1b8d93bea18fd6d14e9e8e40bc2b66d46216a8
Yifaa Yapuncich <yifaasy@gmail.com>
2019-03-26 20:47:00 -0400

    numbers showing up with for loop

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
+'use strict'
+// gets all numbers
 const numberButton = document.querySelectorAll('.number')
+console.log(numberButton)
 
+const oneButton = document.querySelector('.one')
+const twoButton = document.querySelector('.two')
+const threeButton = document.querySelector('.three')
+const fourButton = document.querySelector('.four')
+const fiveButton = document.querySelector('.five')
+const sixButton = document.querySelector('.six')
+const sevenButton = document.querySelector('.seven')
+const eightButton = document.querySelector('.eight')
+const nineButton = document.querySelector('.nine')
+const zeroButton = document.querySelector('.zero')
+// blank line
 let outputLine = document.querySelector('.blank')
 
-numberButton.addEventListener('click', function () {
-    outputLine.innerHTML.replace(numberButton.innerText)
+// displays number when clicked in blank div
+for (let number of numberButton) {
+    number.addEventListener('click', function () {
+    outputLine.innerText += number.innerText
 })
+}
+
+// use eval or math.eval for the math
