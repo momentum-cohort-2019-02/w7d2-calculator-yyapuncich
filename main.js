2271bced8acd96c587c59ea8e2cc34d08e09c98b
Yifaa Yapuncich <yifaasy@gmail.com>
2019-03-26 21:26:38 -0400

    basic math working

--- main.js
+++ main.js
@@ -1,26 +1,45 @@
 'use strict'
 // gets all numbers
 const numberButton = document.querySelectorAll('.number')
-console.log(numberButton)
 
-const oneButton = document.querySelector('.one')
-const twoButton = document.querySelector('.two')
-const threeButton = document.querySelector('.three')
-const fourButton = document.querySelector('.four')
-const fiveButton = document.querySelector('.five')
-const sixButton = document.querySelector('.six')
-const sevenButton = document.querySelector('.seven')
-const eightButton = document.querySelector('.eight')
-const nineButton = document.querySelector('.nine')
-const zeroButton = document.querySelector('.zero')
+// const oneButton = document.querySelector('.one')
+// const twoButton = document.querySelector('.two')
+// const threeButton = document.querySelector('.three')
+// const fourButton = document.querySelector('.four')
+// const fiveButton = document.querySelector('.five')
+// const sixButton = document.querySelector('.six')
+// const sevenButton = document.querySelector('.seven')
+// const eightButton = document.querySelector('.eight')
+// const nineButton = document.querySelector('.nine')
+// const zeroButton = document.querySelector('.zero')
+
+// calc symbols and functions
+const clearButton = document.querySelector('.clear')
+const equalButton = document.querySelector('.equal')
+const decimalButton = document.querySelector('.dot')
+
+// operands all
+const allButtons = document.querySelectorAll('.p')
+
+// const divideButton = document.querySelector('.divide')
+// const multipyButton = document.querySelector('.multipy')
+// const subtractButton = document.querySelector('.subtract')
+// const addButton = document.querySelector('.add')
+
 // blank line
 let outputLine = document.querySelector('.blank')
 
-// displays number when clicked in blank div
-for (let number of numberButton) {
-    number.addEventListener('click', function () {
-    outputLine.innerText += number.innerText
+// displays contents  when clicked in blank div
+
+for (let button of allButtons) {
+    button.addEventListener('click', function () {
+    outputLine.innerText += button.innerText
 })
 }
+
+// let total = eval(out)
+equalButton.addEventListener('click', function () {
+    return outputLine.innerText = (eval(outputLine.innerText))
+})
 
 // use eval or math.eval for the math
